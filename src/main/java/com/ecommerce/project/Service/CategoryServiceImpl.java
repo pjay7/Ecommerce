e3425a67b635140d9daa5109425946bb69b336d8
Jay Patel <jay60436@gmail.com>
2025-04-30 22:57:56 +0530

    Category Crud complete

--- src/main/java/com/ecommerce/project/Service/CategoryServiceImpl.java
+++ src/main/java/com/ecommerce/project/Service/CategoryServiceImpl.java
@@ -6,17 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
-
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-//import java.util.ReverseOrderListView;
 
 @Service
 public class CategoryServiceImpl implements CategoryService {
 
 //    private List<Category> categories = new ArrayList<>();
-    private Long nextId =1L;
+//    private Long nextId =1L;
 
     @Autowired
     private CategoryRepository categoryRepository;
@@ -34,31 +31,20 @@ public class CategoryServiceImpl implements CategoryService {
 
     @Override
     public String deleteCategory(Long categoryId) {
-        List<Category> categories = categoryRepository.findAll();
-
-        Category category = categories.stream()
-                .filter(c -> c.getCategoryId().equals(categoryId))
-                .findFirst()
-                .orElseThrow(()->new ResponseStatusException(HttpStatus.NOT_FOUND,"Resource not found"));
-
+        Category category = categoryRepository.findById(categoryId)
+                .orElseThrow(()->new ResponseStatusException(HttpStatus.NOT_FOUND,"Resource not found")) ;
         categoryRepository.delete(category);
         return "category with categoryId " +categoryId +" deleted successfully!!";
     }
 
     @Override
     public Category updateCategory(Category category, Long categoryId) {
-        List<Category> categories = categoryRepository.findAll();
-        Optional<Category> optionalCategory = categories.stream()
-                .filter(c -> c.getCategoryId().equals(categoryId))
-                .findFirst();
-        if(optionalCategory.isPresent()){
-            Category existingCategory = optionalCategory.get();
-            existingCategory.setCategoryName(category.getCategoryName());
-            Category savedCategory =categoryRepository.save(existingCategory);
-            return savedCategory;
-        }
-        else {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Category not found");
-        }
+
+        Category savedCategory = categoryRepository.findById(categoryId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,"Resource not found"));
+
+        category.setCategoryId(categoryId);
+        savedCategory = categoryRepository.save(category);
+        return savedCategory;
     }
 }
